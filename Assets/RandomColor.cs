af2991401a735560152d0229d239c78ce0d3221e
GMCfromhell <gabrielmoraiscorrea@gmail.com>
2019-05-31 21:31:39 -0300

    Update doido

---
+++ Assets/RandomColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour {
+
+    public SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        spriteRenderer.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+}
